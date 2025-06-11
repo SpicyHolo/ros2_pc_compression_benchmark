14a3057fab4572b57f46d65496a978765bb1c7f6
Konrad Makowski <41269364+SpicyHolo@users.noreply.github.com>
2025-06-11 15:56:19 +0200

    Add development flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Minimal Python flake with LSP.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        pythonEnv = pkgs.python311.withPackages (ps: with ps; []);
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            pkgs.pyright
+           ];
+
+        shellHook = ''
+          echo "Python dev environment read."
+        '';
+
+        };
+      }
+  );
+}
